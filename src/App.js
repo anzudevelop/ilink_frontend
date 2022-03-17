b19e00e44aae01435be7ad2a1567e5dc149f5a4b
alexkosogor <alexk-00@mail.ru>
2022-03-17 11:31:16 +0700

    addingFonts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import s from './App.module.css';
+import './Fonts.css'
 import Header from './components/Header/Header'
 import Content from "./components/Content/Content"
 import Footer from "./components/Footer/Footer"
 
 function App() {
   return (
-      <div className={s.appWrapper}>
+      <div>
           <Header />
           <Content />
           <Footer />
